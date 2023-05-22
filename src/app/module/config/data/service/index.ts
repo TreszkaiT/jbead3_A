c7ed50293fedf7c9f2bfb0d9654990685f3f638e
TreszkaiT <treszkaitom@gmail.com>
2023-05-22 16:48:17 +0200

    Add: user-data.service.impl.ts

--- src/app/module/config/data/service/index.ts
+++ src/app/module/config/data/service/index.ts
@@ -1,1 +1,2 @@
 export * from './config-data.service.mock';
+export * from './config-data.service.impl';
--- src/app/module/user/data/user-data.module.ts
+++ src/app/module/user/data/user-data.module.ts
@@ -4,14 +4,14 @@ import { UserDataService } from 'src/app/api/user';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { UserDataServiceMock } from './services';
+import { UserDataServiceMock, UserDataServiceImpl} from './services';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
   providers: [
     {
       provide: UserDataService,
-      useClass: UserDataServiceMock,
+      useClass: UserDataServiceImpl,
     },
   ],
 })
